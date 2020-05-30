5c74a6652ecd2a7bf32835d14ad89d54d8596ea6
Ricardo David <ricdavid.icm@hotmail.com>
2020-05-30 18:40:57 -0300

    Projeto Criado

---
+++ src/main/java/com/ricardodavid/mvclombok/MvcLombokApplication.java
@@ -0,0 +1,13 @@
+package com.ricardodavid.mvclombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcLombokApplication.class, args);
+	}
+
+}
